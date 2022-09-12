78342f86d25d4b777abf540a33dc9bd2dbb5adbc
aronaldo9 <104202541+aronaldo9@users.noreply.github.com>
2022-09-12 19:40:25 +0200

    Add files via upload

---
+++ src/EmptyTodos/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function EmptyTodos() {
+    return <p>¡Añade tu primera tarea!</p>;
+}
+
+export {EmptyTodos};
---
+++ src/TodosError/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TodosError({ error }) {
+    return <p>OH, OH, ha habido un error, lo sentimos...</p>
+}
+
+export { TodosError };
